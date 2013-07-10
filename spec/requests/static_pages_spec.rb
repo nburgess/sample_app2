7585b6cbb03641d91d7bff8d4ca2f029aef7afc2
nic <your.email@example.com>
2013-07-10 15:45:48 -0400

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,37 @@
+
 require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+      click_link "Help"
+      page.should # fill in
+      click_link "Contact"
+      page.should # fill in
+      click_link "Home"
+      click_link "Sign up now!"
+      page.should # fill in
+      click_link "sample app"
+      page.should # fill in
+    end
+  end
+
   describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', text: 'Sample App')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', text: '| Home')
-    end
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
@@ -58,8 +71,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit contact_path
-      page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
