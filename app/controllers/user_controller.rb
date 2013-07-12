1a737a48aa3ef50790d933a711a2970abd5d3388
nic <your.email@example.com>
2013-07-12 14:44:32 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-
+before_filter :signed_in_user, only: [:edit, :update]
+ before_filter :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
 
   def new
+  end
+ def edit
   end
   def create
     @user = User.new(params[:user])
@@ -15,6 +18,18 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-end
-end
 
+private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710184237) do
+ActiveRecord::Schema.define(:version => 20130712182122) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130710184237) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
